f5e628a679dfe74de8ea41c06feba259b77221e2
jfb5557 <jfb5557@psu.edu>
2020-04-22 19:12:22 -0400

    Added get posts for individual users

--- feeds-script.js
+++ feeds-script.js
@@ -36,10 +36,101 @@ $(document).ready(function() {
         console.log(selectedIndex);
         if(selectedIndex == "0") {
             $("#individual-posts").show();
+            getIndividualPosts();
         }
         else {
             let groupID = document.getElementById("page-selector").options[selectedIndex].text;
             $("#group-div-"+groupID).show();
         }
     });
+
+    function getIndividualPosts()
+    {
+        $.ajax('/individualfeed/get?userID=' + userID,
+            {
+                success: function(posts) {
+                    for (const _post of posts) {
+                        // create a new div that a single post can be placed into
+                        let new_post_div = $("<post></post>");
+                        new_post_div.attr("id", "post-div-" + _post.post.postID.toString());
+                        $("#individual-posts").append(new_post_div);
+
+                        let new_activity_label = $("<label></label>");
+                        new_activity_label.text("Title: " + _post.post.activity.title);
+                        new_activity_label.attr("id", "activity-label-title");
+                        $("#post-div-" + _post.post.postID.toString()).append(new_activity_label);
+
+                        let text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+
+                        new_activity_label = $("<label></label>");
+                        new_activity_label.text("Description: " + _post.post.activity.description);
+                        new_activity_label.attr("id", "activity-label-description");
+                        $("#post-div-" + _post.post.postID.toString()).append(new_activity_label);
+
+                        text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+
+                        new_activity_label = $("<label></label>");
+                        new_activity_label.text("Distance: " + _post.post.activity.distance.toString());
+                        new_activity_label.attr("id", "activity-label-distance");
+                        $("#post-div-" + _post.post.postID.toString()).append(new_activity_label);
+
+                        text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+
+                        new_activity_label = $("<label></label>");
+                        new_activity_label.text("Time Elapsed: " + _post.post.activity.hours.toString() + ":" +
+                            _post.post.activity.minutes.toString() + ":" +
+                            _post.post.activity.seconds.toString());
+                        new_activity_label.attr("id", "activity-label-time_elapsed");
+                        $("#post-div-" + _post.post.postID.toString()).append(new_activity_label);
+
+                        text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+
+                        new_activity_label = $("<label></label>");
+                        new_activity_label.text("Likes: " + _post.post.likes.toString());
+                        new_activity_label.attr("id", "post-label-likes");
+                        $("#post-div-" + _post.post.postID.toString()).append(new_activity_label);
+
+                        text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+                        text_break = $("<br>");
+                        $("#post-div-" + _post.post.postID.toString()).append(text_break);
+
+                    }
+
+                    // for(const item of posts[postID].post.activity)
+                    // {
+                    //     alert("HELLO");
+                    //     if(!(index == 0 || index == 1))
+                    //     {
+                    //         alert("YOU IN");
+                    //         let new_activity_item = $("<label></label>");
+                    //         if(typeof stringValue)
+                    //         {
+                    //             new_activity_item.text(descriptors[index - 2] + element);
+                    //         }
+                    //         else
+                    //         {
+                    //             new_activity_item.text(descriptors[index - 2] + element.toString());
+                    //         }
+                    //         new_activity_item.attr("id","activity-item-" + (index - 2).toString())
+                    //         $("#post-div-" + postID).append(new_activity_item);
+                    //     }
+                    // }
+                    // "Title: " + posts[postID].post.activity.title  +
+                    // "Description: " + posts[postID].post.activity.description +
+                    // "Distance: " + posts[postID].post.activity.distance.toString() +
+                    // "Hours: " + posts[postID].post.activity.hours.toString() +
+                    // "Minutes: " + posts[postID].post.activity.minutes.toString() +
+                    // "Seconds: " + posts[postID].post.activity.seconds.toString() +
+                    // "Likes: " + posts[postID].post.likes.toString() );
+                },
+                error: function() {
+                    alert("Error");
+                }
+            });
+    }
 });
